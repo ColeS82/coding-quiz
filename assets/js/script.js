592c1ddba4d2d6aea0c6cdf2037953038747b6b4
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-09-22 23:21:48 -0700

    Timer works.  Timer ends quiz.  End of quiz ends timer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 
 
 //global variables assigned and declared
-let nextButton = document.querySelector('#next_button')
+let nextButton = document.querySelector('#next_button');
 let startButton = document.querySelector("#start_button");
+let resultsButton = document.querySelector('#see_results')
 let questionElement = document.querySelector('#questions');
 let mainCard = document.querySelector('#main_card');
 let answerContainer = document.querySelector('#answer_container');
 let answerList = document.querySelector("#answer_list");
-let secondsLeft = 10
+let secondsLeft = 15
 let time = document.querySelector("#time")
+let clock = document.querySelector('#clock')
+let resultsCard = document.querySelector('#results_card')
 
 
 //This assigns but does not define.  Will be defined in later functions
@@ -47,37 +50,52 @@ const q_and_a = [
 
 //invokes the startQuiz funtion when start button is clecked.
 startButton.addEventListener('click', startQuiz);
-startButton.addEventListener('click', setTime);
 
 //Sets eventlistner to the nextbutton to move to the next question
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     nextQuestion()
 })
-
-//This function starts the quiz by removing the hide class from the answercontainer and randomizing the questions to be inserted.
+// starts countdown timer and ends quiz at 0 seconds.
 function setTime() { 
     timerInterval = setInterval(function () {
         secondsLeft--;
         time.innerText = secondsLeft + " Seconds left";
         if (secondsLeft === 0) {
+            resetState(questionElement.innerText = "END OF QUIZ");
             clearInterval(timerInterval);
-            return;
+            alert('Times up!')
+            secondsLeft = 15;
+            resultsButton.classList.remove('hide')
         }
+        resultsButton.addEventListener('click', showResults)
     }, 1000);
 }
+//removes the #main_card and #clock element.  I don't know why I have to explicitly target and style these vs. .classList.add('hide') for clock and mainCard variables... see comment on line 14 in index.html.
+function showResults(){
+    document.getElementById("clock").style.display = 'none';
+    document.getElementById('main_card').style.display = 'none';
+    resultsCard.classList.remove('hide');
+    startButton.innerText = 'restart';
+    startButton.classList.remove('hide')
+    resultsButton.classList.add('hide')
+}
 
-
+//This function starts the quiz by removing the hide class from the answercontainer and randomizing the questions to be inserted.
 function startQuiz() {
+        document.getElementById("clock").style.display = 'flex';
+        document.getElementById('main_card').style.display = 'flex';
+        resultsCard.classList.add('hide');
         console.log('testing');
         answerContainer.classList.remove('hide');
         shuffled = q_and_a.sort(() => Math.random() - .5);
         currentQuestionIndex = 0;
         startButton.classList.add('hide');
+        setTime()
         nextQuestion();
     }
 
-
+//envokes showQuestion which then inserts the randomized question with associated answers.
 function nextQuestion() {
     resetState()
     showQuestion(shuffled[currentQuestionIndex])
@@ -97,6 +115,7 @@ function showQuestion(question){
     });
 }
 
+//prevents answers list form accumulating
 function resetState() {
     nextButton.classList.add("hide")
     while (answerList.firstChild) {
@@ -105,6 +124,7 @@ function resetState() {
     }
 }
 
+//selects answer and shows the correct answer in green and wrong answer in red.
 function selectAnswer(e) {
     Array.from(answerList.children).forEach(selection => {
         setStatusClass(selection, selection.dataset.correct)
@@ -112,11 +132,18 @@ function selectAnswer(e) {
     if (shuffled.length > currentQuestionIndex + 1){
         nextButton.classList.remove('hide')
         }else {
-            startButton.innerText = 'See Results'
-            startButton.classList.remove('hide')
+            resetTime()
+            resetState(questionElement.innerText = "END OF QUIZ");
+            resultsButton.classList.remove('hide')
         }
-        startButton.addEventListener('click', results)
+        resultsButton.addEventListener('click', showResults)
     }
+
+function resetTime(){
+        resetState(questionElement.innerText = "END OF QUIZ");
+        clearInterval(timerInterval);
+        secondsLeft = 15;
+}
 
 function setStatusClass(element, correct){
     clearStatusClass(element)
